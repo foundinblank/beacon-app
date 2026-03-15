c4f2fccd2c5cfbcbe4e76a3b55e5751b023cd48c
Adam Stone <adamstone@gmail.com>
2026-03-15 10:02:04 -0400

    feat(renderer): SpotlightRenderer reads masterColor when sync is on

--- Beacon/Beacon/Overlay/SpotlightRenderer.swift
+++ Beacon/Beacon/Overlay/SpotlightRenderer.swift
@@ -85,7 +85,12 @@ class SpotlightRenderer {
             ?? SettingsDefaults.spotlightDimOpacity)
         let borderWidth = CGFloat(defaults.object(forKey: SettingsKeys.spotlightBorderWidth) as? Double
             ?? SettingsDefaults.spotlightBorderWidth)
-        let colorHex = defaults.string(forKey: SettingsKeys.spotlightBorderColor) ?? SettingsDefaults.spotlightBorderColor
+        let colorHex: String
+        if defaults.object(forKey: SettingsKeys.syncColor) as? Bool ?? SettingsDefaults.syncColor {
+            colorHex = defaults.string(forKey: SettingsKeys.masterColor) ?? SettingsDefaults.masterColor
+        } else {
+            colorHex = defaults.string(forKey: SettingsKeys.spotlightBorderColor) ?? SettingsDefaults.spotlightBorderColor
+        }
 
         if enabled == lastEnabled && radius == lastRadius &&
             dimOpacity == lastDimOpacity && borderWidth == lastBorderWidth &&
